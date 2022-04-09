1d3f27df6c86af520f0e4a945c1ba59d58be8e78
TypeFloat <liuzekun_123@163.com>
2022-04-09 10:53:51 +0800

    init commit

--- setup.py
+++ setup.py
@@ -7,8 +7,8 @@ setup(
     author="TypeFloat",
     author_email="liuzekun_123@163.com",
     description="Help you use hardware easily with python.",
-    url="http://iswbm.com/", 
+    url="https://github.com/TypeFloat/easy-hardware", 
     packages=["easyhardware"],
-    python_requires=">=3.6"
+    python_requires=">=3.6",
     install_requires=["smbus>=1.1.post2"]
 )
